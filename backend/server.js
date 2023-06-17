5bb31d524f1c4a517f9fed9eeef6593befa196f5
yugurlu <yusufugurlu39@outlook.com>
2023-06-17 18:01:15 +0300

    son

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,22 @@
 
-var http = require("http")
-var fs = require("fs")
 
-var server = http.createServer(function(req,res){
-    if(req.url == '/'){
-        fs.readFile('index.html', function(err,data){
-            res.write(data)
-            res.end()
-        })
-    }
-    else if(req.url == '/login'){
-        res.write("login screen")
+var fs = require("fs")
+var path = require("path")
+var express = require("express")
+var app = express();
+
+app.use('/public', express.static(path.join(__dirname, "public")))
+
+app.get('/', function(req, res){
+    fs.readFile('index.html', function(err,data){
+        res.write(data)
         res.end()
-    }
+    })
 })
 
-server.listen(8000)
+app.get('/login', function(req, res){
+    res.write("login screen")
+    res.end()
+})
+
+app.listen(8000)
