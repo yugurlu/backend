30d55a0ee032824bf203450459497d3aa8e8ce19
yugurlu <yusufugurlu39@outlook.com>
2023-08-03 19:17:00 +0300

    rest_api

--- nestjs/deneme/src/users/user.services.ts
+++ nestjs/deneme/src/users/user.services.ts
@@ -1,15 +1,33 @@
 
 import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
-import { User, UserDocument } from "./schemas/users.schemas";
+import { User, UserDocument } from './schemas/users.schemas';
 import { Model } from "mongoose";
+import { CreatePersonDto } from "./dto/create.users.dto";
 
 
 @Injectable()
 export class UsersService {
    constructor(@InjectModel(User.name) private userModel : Model<UserDocument>) {}
 
-    async findall() {
-        return this.userModel.find()
+    async getAll(): Promise<User[]> {
+        return await this.userModel.find()
+    }
+
+    async getOne(id: string): Promise<User> {
+        return await this.userModel.findById(id)
+    }
+
+    async create(allProps: CreatePersonDto): Promise<User> {
+        const user = new this.userModel(allProps)
+        return await user.save()
+    }
+
+    async update(id: string, allProps: CreatePersonDto): Promise<User> {
+        return await this.userModel.findByIdAndUpdate(id, allProps, { new: true })
+    }
+
+    async remove(id: string): Promise<User> {
+        return await this.userModel.findByIdAndDelete(id)
     }
 }
--- nestjs/deneme/src/users/users.controller.ts
+++ nestjs/deneme/src/users/users.controller.ts
@@ -1,24 +1,37 @@
 
-import { Body, Controller, Get, Post, Param } from '@nestjs/common'
+import { Body, Controller, Get, Post, Param, Put, Delete } from '@nestjs/common'
 import { CreatePersonDto } from './dto/create.users.dto'
 import { UsersService } from './user.services';
+import { ParamsTokenFactory } from '@nestjs/core/pipes';
+import { User } from './schemas/users.schemas';
 
 @Controller("users")
 export class UsersController {
     constructor(private usersService: UsersService) {}
 
     @Get()
-    getall() {
-        return this.usersService.findall()
+    getall(): Promise<User[]> {
+        return this.usersService.getAll()
     }
 
     @Get(":id")
-    getOne(@Param("id") id) {
-        return `this id ${id}`
+    getOne(@Param("id") id: string): Promise<User> {
+        return this.usersService.getOne(id)
     }
 
     @Post()
-    create(@Body() person: CreatePersonDto) {
-        return `User created!\nUsername: ${person.username}\nAge: ${person.age}`
+    create(@Body() allProps: CreatePersonDto): Promise<User> {
+        return this.usersService.create(allProps)
     }
+
+    @Put(":id")
+    update(@Param("id") id: string, @Body() allProps: CreatePersonDto): Promise<User> {
+        return this.usersService.update(id, allProps)
+    }
+
+    @Delete(":id")
+    remove(@Param("id") id: string): Promise<User> {
+        return this.usersService.remove(id)
+    }
+
 } 
